c2c9fff028fa4ed759a7a931a937503fc073a3c8
VolunteerNow <kevin.robinson@volunteernow.co.uk>
2017-01-20 17:29:08 +0000

    Added Karma PhantomJs launcher

--- karma.conf.js
+++ karma.conf.js
@@ -4,159 +4,161 @@ module.exports = function (config) {
 
     var options = {
         files: [].concat(
-           // bower:js
-           "bower_components/angular/angular.js",
-           "bower_components/angular-animate/angular-animate.js",
-           "bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
-           "bower_components/angular-sanitize/angular-sanitize.js",
-           "bower_components/angular-ui-select/dist/select.js",
-           "bower_components/angular-messages/angular-messages.js",
-           "bower_components/ng-fab-form/dist/ng-fab-form.js",
-           "bower_components/angular-ui-router/release/angular-ui-router.js",
-           "bower_components/auth0-lock/build/lock.js",
-           "bower_components/angular-lock/angular-lock.js",
-           "bower_components/angular-jwt/dist/angular-jwt.js",
-           "bower_components/angular-ui-ace/ui-ace.js",
-           "bower_components/angular-showdown/showdown.js",
-           "bower_components/angular-ui-grid/ui-grid.js",
-           "bower_components/jquery/dist/jquery.js",
-           "bower_components/bootstrap/dist/js/bootstrap.js",
-           "bower_components/firebase/firebase.js",
-           "bower_components/angularfire/dist/angularfire.js",
-           "bower_components/angular-mocks/angular-mocks.js",
-           "bower_components/sinon/index.js",
-           "bower_components/bardjs/dist/bard.js",
-           "bower_components/bardjs/dist/bard-ngRouteTester.js",
-           // endbower
+            // bower:js
+            "bower_components/angular/angular.js",
+            "bower_components/angular-animate/angular-animate.js",
+            "bower_components/angular-bootstrap/ui-bootstrap-tpls.js",
+            "bower_components/angular-sanitize/angular-sanitize.js",
+            "bower_components/angular-ui-select/dist/select.js",
+            "bower_components/angular-messages/angular-messages.js",
+            "bower_components/ng-fab-form/dist/ng-fab-form.js",
+            "bower_components/angular-ui-router/release/angular-ui-router.js",
+            "bower_components/auth0-lock/build/lock.js",
+            "bower_components/angular-lock/angular-lock.js",
+            "bower_components/angular-jwt/dist/angular-jwt.js",
+            "bower_components/angular-ui-ace/ui-ace.js",
+            "bower_components/angular-showdown/showdown.js",
+            "bower_components/angular-ui-grid/ui-grid.js",
+            "bower_components/jquery/dist/jquery.js",
+            "bower_components/bootstrap/dist/js/bootstrap.js",
+            "bower_components/firebase/firebase.js",
+            "bower_components/angularfire/dist/angularfire.js",
+            "bower_components/angular-mocks/angular-mocks.js",
+            "bower_components/sinon/index.js",
+            "bower_components/bardjs/dist/bard.js",
+            "bower_components/bardjs/dist/bard-ngRouteTester.js",
+            // endbower
 
-    // inject:js
-    "src/Client/App/Components/app.js",
-    "src/Client/App/Components/cssClassService.js",
-    "src/Client/App/Components/Buttons/CreateButton/createButton.js",
-    "src/Client/App/Components/Buttons/SpinnerButton/spinnerButton.js",
-    "src/Client/App/Components/ComboBoxes/CategorySelect/categorySelect.js",
-    "src/Client/App/Components/Filters/FilterTextbox/filterTextbox.js",
-    "src/Client/App/Components/Headers/PageHeader/pageHeader.js",
-    "src/Client/App/Components/Inputs/DateTimeDifferenceField/dateTimeDifferenceField.js",
-    "src/Client/App/Components/Inputs/InputField/inputField.js",
-    "src/Client/App/Components/Inputs/SelectField/selectField.js",
-    "src/Client/App/Components/Inputs/TextEditor/markdown.directive.js",
-    "src/Client/App/Components/Inputs/TextEditor/textEditor.js",
-    "src/Client/App/Components/Modals/ModalButtons/modalButtons.js",
-    "src/Client/App/Components/Modals/ModalHeader/modalHeader.js",
-    "src/Client/App/Components/Panels/InfoPanel/infoPanel.js",
-    "src/Client/App/Components/Panels/MoreLessButton/moreLessButton.js",
-    "src/Client/App/Components/Panels/MoreLessPanel/moreLessPanel.js",
-    "src/Client/App/Components/Panels/Panel/panel.js",
-    "src/Client/App/Components/StatusAlerts/StatusAlert/statusAlert.js",
-    "src/Client/App/Components/Inputs/DatesField/DateField/dateField.js",
-    "src/Client/App/Components/Layout/AdminLayout/AdminLayoutCore/adminLayout.js",
-    "src/Client/App/Components/Layout/AdminLayout/Header/AlertsDropDown/alertsDropDownMenu.js",
-    "src/Client/App/Components/Layout/AdminLayout/Header/HeaderBar/headerBar.js",
-    "src/Client/App/Components/Layout/AdminLayout/Header/UserOptionsDropDown/userOptionsDropDownMenu.js",
-    "src/Client/App/Components/Layout/AdminLayout/SideMenu/SideMenu/sideMenu.js",
-    "src/Client/App/Components/Layout/AdminLayout/SideMenu/SideMenuFooter/sideMenuFooter.js",
-    "src/Client/App/Components/Layout/AdminLayout/SideMenu/SideMenuItems/sideMenuItems.js",
-    // endinject
+            // inject:js
+            "src/Client/App/Components/app.js",
+            "src/Client/App/Components/cssClassService.js",
+            "src/Client/App/Components/Buttons/CreateButton/createButton.js",
+            "src/Client/App/Components/Buttons/SpinnerButton/spinnerButton.js",
+            "src/Client/App/Components/ComboBoxes/CategorySelect/categorySelect.js",
+            "src/Client/App/Components/Filters/FilterTextbox/filterTextbox.js",
+            "src/Client/App/Components/Headers/PageHeader/pageHeader.js",
+            "src/Client/App/Components/Inputs/DateTimeDifferenceField/dateTimeDifferenceField.js",
+            "src/Client/App/Components/Inputs/InputField/inputField.js",
+            "src/Client/App/Components/Inputs/SelectField/selectField.js",
+            "src/Client/App/Components/Inputs/TextEditor/markdown.directive.js",
+            "src/Client/App/Components/Inputs/TextEditor/textEditor.js",
+            "src/Client/App/Components/Modals/ModalButtons/modalButtons.js",
+            "src/Client/App/Components/Modals/ModalHeader/modalHeader.js",
+            "src/Client/App/Components/Panels/InfoPanel/infoPanel.js",
+            "src/Client/App/Components/Panels/MoreLessButton/moreLessButton.js",
+            "src/Client/App/Components/Panels/MoreLessPanel/moreLessPanel.js",
+            "src/Client/App/Components/Panels/Panel/panel.js",
+            "src/Client/App/Components/StatusAlerts/StatusAlert/statusAlert.js",
+            "src/Client/App/Components/Inputs/DatesField/DateField/dateField.js",
+            "src/Client/App/Components/Layout/AdminLayout/AdminLayoutCore/adminLayout.js",
+            "src/Client/App/Components/Layout/AdminLayout/Header/AlertsDropDown/alertsDropDownMenu.js",
+            "src/Client/App/Components/Layout/AdminLayout/Header/HeaderBar/headerBar.js",
+            "src/Client/App/Components/Layout/AdminLayout/Header/UserOptionsDropDown/userOptionsDropDownMenu.js",
+            "src/Client/App/Components/Layout/AdminLayout/SideMenu/SideMenu/sideMenu.js",
+            "src/Client/App/Components/Layout/AdminLayout/SideMenu/SideMenuFooter/sideMenuFooter.js",
+            "src/Client/App/Components/Layout/AdminLayout/SideMenu/SideMenuItems/sideMenuItems.js",
+            // endinject
 
- // inject:examplesjs
-    
-    "src/Client/App/Examples/app.js",
-    
-    "src/Client/App/Examples/appconfig.js",
-    
-    "src/Client/App/Examples/auth.service.js",
-    
-    "src/Client/App/Examples/login.controller.js",
-    
-    "src/Client/App/Examples/routes.js",
-    
-    "src/Client/App/Examples/Common/common.js",
-    
-    "src/Client/App/Examples/Dates/dateExamples.js",
-    
-    "src/Client/App/Examples/Firebase/firebase.js",
-    
-    "src/Client/App/Examples/ExampleForm/exampleForm.js",
-    
-    "src/Client/App/Examples/Grid/gridExample.js",
-    
-    "src/Client/App/Examples/Inputs/inputs.js",
-    
-    "src/Client/App/Examples/Layout/adminLayoutExample.js",
-    
-    "src/Client/App/Examples/Map/mapExample.js",
-    
-    "src/Client/App/Examples/Modals/modals.js",
-    
-    "src/Client/App/Examples/Panels/panels.js",
-    
-    "src/Client/App/Examples/SelectLists/selectListExamples.js",
-    
-    "src/Client/App/Examples/StatusAlerts/statusAlerts.js",
-    
-    "src/Client/App/Examples/Tags/tags.js",
-    
-    "src/Client/App/Examples/TextEditor/textEditorExample.js",
-    
-    "src/Client/App/Examples/UserProfile/userProfile.js",
-    
-    "src/Client/App/Examples/ExampleForm/CreateButton/createButton.js",
-    
-    "src/Client/App/Examples/Other/Spinner/spinner.js",
-    
-    "src/Client/App/Examples/Other/TextEditor/markdown.directive.js",
-    
-    "src/Client/App/Examples/Other/TextEditor/textEditor.js",
-    
-    // endinject
+            // inject:examplesjs
 
-    // spec:js
-   
-    "src/Client/tests/Components/Buttons/spinnerButton.spec.js",
-   
-    "src/Client/tests/Components/Headers/pageHeader.spec.js",
-   
-    "src/Client/tests/Components/Inputs/dateField.spec.js",
-   
-    "src/Client/tests/Components/Inputs/inputField.spec.js",
-   
-    "src/Client/tests/Components/Inputs/selectField.spec.js",
-   
-    "src/Client/tests/Components/Inputs/textEditor.spec.js",
-   
-    "src/Client/tests/Components/Layout/layoutCore.spec.js",
-   
-    "src/Client/tests/Components/Panels/infoPanel.spec.js",
-   
-    "src/Client/tests/Components/Panels/moreLessButton.spec.js",
-   
-    "src/Client/tests/Components/Panels/moreLessPanel.spec.js",
-   
-    "src/Client/tests/Components/Panels/panel.spec.js",
-   
-    "src/Client/tests/Components/Modals/modalButtons.spec.js",
-   
-    "src/Client/tests/Components/Modals/modalHeader.spec.js",
-   
-    "src/Client/tests/Components/StatusAlerts/statusAlert.spec.js",
-   
-    "src/Client/tests/Components/Layout/Header/alertsDropDown.spec.js",
-   
-    "src/Client/tests/Components/Layout/Header/headerBar.spec.js",
-   
-    "src/Client/tests/Components/Layout/Header/userOptionsDropDown.spec.js",
-   
-    "src/Client/tests/Components/Layout/SideMenu/sideMenu.spec.js",
-   
-    "src/Client/tests/Components/Layout/SideMenu/sideMenuFooter.spec.js",
-   
-    "src/Client/tests/Components/Layout/SideMenu/sideMenuItems.spec.js"
-   
-    // endinject
+            "src/Client/App/Examples/app.js",
 
-    // spec:examplesjs
-    // endinject
+            "src/Client/App/Examples/appconfig.js",
+
+            "src/Client/App/Examples/auth.service.js",
+
+            "src/Client/App/Examples/login.controller.js",
+
+            "src/Client/App/Examples/routes.js",
+
+            "src/Client/App/Examples/Common/common.js",
+
+            "src/Client/App/Examples/Dates/dateExamples.js",
+
+            "src/Client/App/Examples/ExampleForm/exampleForm.js",
+
+            "src/Client/App/Examples/Firebase/firebase.js",
+
+            "src/Client/App/Examples/Grid/gridExample.js",
+
+            "src/Client/App/Examples/Home/home.js",
+
+            "src/Client/App/Examples/Layout/adminLayoutExample.js",
+
+            "src/Client/App/Examples/Inputs/inputs.js",
+
+            "src/Client/App/Examples/Map/mapExample.js",
+
+            "src/Client/App/Examples/Modals/modals.js",
+
+            "src/Client/App/Examples/Panels/panels.js",
+
+            "src/Client/App/Examples/SelectLists/selectListExamples.js",
+
+            "src/Client/App/Examples/Tags/tags.js",
+
+            "src/Client/App/Examples/TextEditor/textEditorExample.js",
+
+            "src/Client/App/Examples/StatusAlerts/statusAlerts.js",
+
+            "src/Client/App/Examples/UserProfile/userProfile.js",
+
+            "src/Client/App/Examples/ExampleForm/CreateButton/createButton.js",
+
+            "src/Client/App/Examples/Other/Spinner/spinner.js",
+
+            "src/Client/App/Examples/Other/TextEditor/markdown.directive.js",
+
+            "src/Client/App/Examples/Other/TextEditor/textEditor.js",
+
+            // endinject
+
+            // spec:js
+
+            "src/Client/tests/Components/Buttons/spinnerButton.spec.js",
+
+            "src/Client/tests/Components/Headers/pageHeader.spec.js",
+
+            "src/Client/tests/Components/Inputs/dateField.spec.js",
+
+            "src/Client/tests/Components/Inputs/inputField.spec.js",
+
+            "src/Client/tests/Components/Inputs/selectField.spec.js",
+
+            "src/Client/tests/Components/Inputs/textEditor.spec.js",
+
+            "src/Client/tests/Components/Layout/layoutCore.spec.js",
+
+            "src/Client/tests/Components/Modals/modalButtons.spec.js",
+
+            "src/Client/tests/Components/Modals/modalHeader.spec.js",
+
+            "src/Client/tests/Components/Panels/infoPanel.spec.js",
+
+            "src/Client/tests/Components/Panels/moreLessButton.spec.js",
+
+            "src/Client/tests/Components/Panels/moreLessPanel.spec.js",
+
+            "src/Client/tests/Components/Panels/panel.spec.js",
+
+            "src/Client/tests/Components/StatusAlerts/statusAlert.spec.js",
+
+            "src/Client/tests/Components/Layout/Header/alertsDropDown.spec.js",
+
+            "src/Client/tests/Components/Layout/Header/headerBar.spec.js",
+
+            "src/Client/tests/Components/Layout/Header/userOptionsDropDown.spec.js",
+
+            "src/Client/tests/Components/Layout/SideMenu/sideMenu.spec.js",
+
+            "src/Client/tests/Components/Layout/SideMenu/sideMenuFooter.spec.js",
+
+            "src/Client/tests/Components/Layout/SideMenu/sideMenuItems.spec.js"
+
+            // endinject
+
+            // spec:examplesjs
+            // endinject
         ),
         exclude: [],
         // coverage: {
@@ -172,7 +174,16 @@ module.exports = function (config) {
         //         type: 'text-summary'
         //     }]
         // },
-        preprocessors: {}
+        preprocessors: {
+            ngHtml2JsPreprocessor: {
+                cacheIdFromPath: function (filepath) {
+                    //The suggested filepath.strip would through an error
+                    console.llog(filepath);
+                    var cacheId = filepath.replace('main/resources/static/', '');
+                    return cacheId;
+                }
+            }
+        }
     };
 
 
@@ -217,6 +228,7 @@ module.exports = function (config) {
             'karma-jasmine',
             'karma-chai',
             'karma-chrome-launcher',
+            'karma-phantomjs-launcher'
         ],
 
         // enable / disable colors in the output (reporters and logs)
@@ -237,7 +249,7 @@ module.exports = function (config) {
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         //        browsers: ['Chrome', 'ChromeCanary', 'FirefoxAurora', 'Safari', 'PhantomJS'],
-        browsers: ['Chrome'],
+        browsers: ['PhantomJS'],
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
--- src/client/tests/Components/Inputs/inputField.spec.js
+++ src/client/tests/Components/Inputs/inputField.spec.js
@@ -1,4 +1,4 @@
-describe("Components", function () {
+fdescribe("Components", function () {
 
     describe("Inputs", function () {
 
@@ -11,14 +11,64 @@ describe("Components", function () {
 
             //instantiate component controller
             beforeEach(function () {
-                bard.inject(this, '$componentController');
+                bard.inject(this, '$componentController', '$compile', '$rootScope', '$httpBackend');
                 vm = $componentController('myInputField', null);
             });
 
 
             it("should be defined", function () {
-                expect(vm).toBeDefined();
+                // $compile the template, and pass in the $scope.
+             // This will find your directive and run everything
+              $httpBackend.expectGET("inputFieldTemplate.html").respond(200);
+             var template = $compile("<my-input-field input-type='text'></my-input-field>")($rootScope);
+ 
+              
+          
+             // Now run a $digest cycle to update your template with new data
+             $rootScope.$digest();
+             console.log("template");
+            console.log(template);
+             // Render the template as a string
+             var templateAsHtml = template.html();
+            console.log(templateAsHtml);
+            //  // Verify that the $scope variables are in the template
+            //  expect(templateAsHtml).toContain($rootScope.header);
+            //  expect(templateAsHtml).toContain($rootScope.text);
+ 
+            //  // Do it again with new values
+            //  var previousHeader = $rootScope.header;
+            //  var previousText = $scope.text;
+            //  $scope.header = "A completely different header";
+            //  $scope.text = "Something completely different";
+ 
+            //  // Run the $digest cycle again
+            //  $scope.$digest();
+ 
+            //  templateAsHtml = template.html();
+ 
+            //  expect(templateAsHtml).toContain($scope.header);
+            //  expect(templateAsHtml).toContain($scope.text);
+            //  expect(templateAsHtml).toNotContain(previousHeader);
+            //  expect(templateAsHtml).toNotContain(previousText);
+
             });
+
+
+            // it('Replaces the element with the appropriate content', function () {
+            //     //expect a get request to "internalapi/quotes"
+            //     $httpBackend.expectGET("inputFieldTemplate.html").respond(200);
+            //     // Compile a piece of HTML containing the directive
+            //     var element =angular.element("<my-input-field input-type='text'></my-input-field>")($rootScope);
+            //     element = $compile(element)($rootScope);
+            //     // fire all the watches, so the scope expression {{1 + 1}} will be evaluated
+            //     $rootScope.$digest();
+            //     scope.$digest();
+            //         $httpBackend.flush();
+            //     // Check that the compiled element contains the templated content
+            //     console.log("_----------------");
+            //     console.log(element);
+            //     expect(element.html()).toContain("text");
+            // });
         });
 
     });
