a36e70e1c947fe2d5176b9287fa2228d5dd0888d
VolunteerNow <kevin.robinson@volunteernow.co.uk>
2017-02-07 14:31:05 +0000

    added eslint

--- build/js/app.js
+++ build/js/app.js
@@ -1238,6 +1238,26 @@ var adminHeaderBar = {
 
 
 angular.module('my-angular-components').component('adminHeaderBar', adminHeaderBar);
+var userOptionsDropDownMenu = {
+    bindings: {
+        menuItems:'=',
+        userName: '@'
+    },
+    controllerAs: 'vm',
+    controller: function () {
+        'use strict';
+
+        var vm = this;
+        
+       
+
+    },
+    template:'<div class="item dropdown" uib-dropdown><a href="#" class="dropdown-toggle" uib-dropdown-toggle><i class="fa fa-user"></i></a><ul class="dropdown-menu dropdown-menu-right"><li class="dropdown-header">{{vm.userName}}</li><li class="divider"></li><li ng-repeat="item in vm.menuItems"><div class="row"><i class="fa fa-{{item.icon}} col-md-3" style="margin:0"></i> <a ui-sref="{{item.state}}" class="col-md-8">{{item.linkText}}</a></div></li></ul></div>'
+};
+
+
+angular.module('my-angular-components').component('userOptionsDropDownMenu', userOptionsDropDownMenu);
+
 var adminSideMenu = {
     transclude: true,
     bindings: {
@@ -1267,26 +1287,6 @@ var adminSideMenu = {
 
 
 angular.module('my-angular-components').component('adminSideMenu', adminSideMenu);
-
-var userOptionsDropDownMenu = {
-    bindings: {
-        menuItems:'=',
-        userName: '@'
-    },
-    controllerAs: 'vm',
-    controller: function () {
-        'use strict';
-
-        var vm = this;
-        
-       
-
-    },
-    template:'<div class="item dropdown" uib-dropdown><a href="#" class="dropdown-toggle" uib-dropdown-toggle><i class="fa fa-user"></i></a><ul class="dropdown-menu dropdown-menu-right"><li class="dropdown-header">{{vm.userName}}</li><li class="divider"></li><li ng-repeat="item in vm.menuItems"><div class="row"><i class="fa fa-{{item.icon}} col-md-3" style="margin:0"></i> <a ui-sref="{{item.state}}" class="col-md-8">{{item.linkText}}</a></div></li></ul></div>'
-};
-
-
-angular.module('my-angular-components').component('userOptionsDropDownMenu', userOptionsDropDownMenu);
 
 var sideMenuFooter = {
     bindings: {
