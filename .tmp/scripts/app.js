9b2b89e3535411aeaa77e24b4274f40c38df1ba8
VolunteerNow <kevin.robinson@volunteernow.co.uk>
2017-02-09 16:36:39 +0000

    Added Styling to Checkbox and Select

--- .tmp/scripts/app.js
+++ .tmp/scripts/app.js
@@ -2,12 +2,13 @@ var app = angular.module('my-angular-components', [
     //angular
     'ngAnimate',
     'ngSanitize',
-
+    
     //angular ui
     'ui.ace',
     'ui.bootstrap',
     'ui.select',
     'ui.grid',  
+    'ui.checkbox'
 ]);
-app.$inject = ['ngAnimate', 'ngSanitize', 'ui.ace', 'ui.bootstrap', 'ui.select', 'ui.grid'];
+app.$inject = ['ngAnimate', 'ngSanitize', 'ui.ace', 'ui.bootstrap', 'ui.select', 'ui.grid', 'ui.checkbox'];
 
--- build/js/app.js
+++ build/js/app.js
@@ -5,14 +5,15 @@ var app = angular.module('my-angular-components', [
     //angular
     'ngAnimate',
     'ngSanitize',
-
+    
     //angular ui
     'ui.ace',
     'ui.bootstrap',
     'ui.select',
     'ui.grid',  
+    'ui.checkbox'
 ]);
-app.$inject = ['ngAnimate', 'ngSanitize', 'ui.ace', 'ui.bootstrap', 'ui.select', 'ui.grid'];
+app.$inject = ['ngAnimate', 'ngSanitize', 'ui.ace', 'ui.bootstrap', 'ui.select', 'ui.grid', 'ui.checkbox'];
 
 
 var cssClassService =  function () {
@@ -131,29 +132,6 @@ angular.module('my-angular-components').component('mySpinnerButton', mySpinnerBu
 
 
 
-var myFilterTextbox = {
-    bindings: {
-        placeholder: '@',
-        ngModel: '=',
-        fieldName: '@'
-    },
-    controllerAs: 'vm',
-    controller: function () {
-        var vm = this;
-
-        vm.$onInit = function () {
-            vm.fieldName = 'filterTextBox';
-            vm.placeholder = 'Filter';
-        };
-
-
-    },
-    template:'<div class="input-group" style="display: flex"><input type="text" ng-model="vm.ngModel" placeholder="{{vm.placeholder}}" id="{{vm.fieldName}}" class="form-control"> <button class="btn btn-default" id="searchFilter"><i class="glyphicon glyphicon-search"></i></button></div>'
-};
-
-
-angular.module('my-angular-components').component('myFilterTextbox', myFilterTextbox);
-
 /**
  * 
  * @type {
@@ -260,6 +238,29 @@ var buildList = function () {
 myCategorySelect.$inject = ['$scope'];
 
 angular.module('my-angular-components').component('myCategorySelect', myCategorySelect);
+var myFilterTextbox = {
+    bindings: {
+        placeholder: '@',
+        ngModel: '=',
+        fieldName: '@'
+    },
+    controllerAs: 'vm',
+    controller: function () {
+        var vm = this;
+
+        vm.$onInit = function () {
+            vm.fieldName = 'filterTextBox';
+            vm.placeholder = 'Filter';
+        };
+
+
+    },
+    template:'<div class="input-group" style="display: flex"><input type="text" ng-model="vm.ngModel" placeholder="{{vm.placeholder}}" id="{{vm.fieldName}}" class="form-control"> <button class="btn btn-default" id="searchFilter"><i class="glyphicon glyphicon-search"></i></button></div>'
+};
+
+
+angular.module('my-angular-components').component('myFilterTextbox', myFilterTextbox);
+
 var myPageTitle = {
     transclude: "true",
     bindings: {
@@ -411,7 +412,7 @@ var myInputField = {
 
 
     },
-    template:'<style>\r\n    .popover {\r\n        min-width: 200px;\r\n    }\r\n    \r\n    .input-group {\r\n        width: 100% !important;\r\n    }\r\n    \r\n    .input-group .form-control {\r\n        z-index: 100;\r\n    }\r\n</style><div class="form-group"><label ng-class="vm.cssClassService.getlabelClass(vm.horizontal, vm.labelWidth)" for="vm.fieldName">{{vm.fieldLabel}}</label><div ng-class="vm.cssClassService.getInputClass(vm.horizontal, vm.inputWidth)"><div ng-if="!vm.readOnly" class="input-group"><input ng-if="vm.inputType===\'textbox\'" ng-model="vm.ngModel" type="text" class="form-control" id="{{vm.fieldName}}" placeholder="{{vm.placeholder}}" required><input ng-if="vm.inputType===\'number\'" ng-model="vm.ngModel" type="number" class="form-control" id="{{vm.fieldName}}" required><input ng-if="vm.inputType===\'email\'" type="email" class="form-control" id="vm.fieldName" placeholder="{{vm.placeholder}}"><textarea ng-if="vm.inputType===\'textarea\'" class="form-control" id="vm.fieldName" ng-model="vm.ngModel" placeholder="{{vm.placeholder}}"></textarea><input ng-if="vm.inputType===\'checkbox\'" type="checkbox" id="vm.fieldName" ng-model="vm.ngModel"><select ng-if="vm.inputType===\'select\'" class="form-control" ng-model="vm.ngModel" ng-options="item for item in vm.items"></select><input ng-if="vm.inputType===\'time\'" uib-timepicker ng-model="vm.ngModel" id="vm.fieldName" ng-change="vm.changed()" hour-step="vm.hourStep" minute-step="vm.minStep" show-meridian="vm.showMeridian"><span ng-if="vm.inputType===\'datepopup\'"><input type="text" class="form-control" uib-datepicker-popup="{{vm.format}}" ng-model="vm.ngModel" is-open="vm.isOpened" style="width: 82%" ng-required="true" close-text="Close"> <span class="btn btn-default glyphicon glyphicon-calendar" ng-click="vm.open()"></span></span> <span ng-show="vm.tooltip!==\'\'" class="input-group-addon" uib-popover="{{vm.tooltip}}" popover-title="Info" popover-class="popover" popover-trigger="\'mouseenter\'"><i class="fa fa-info"></i></span></div><div ng-show="vm.readOnly"><p ng-if="vm.inputType!==\'checkbox\'">{{vm.ngModel}}</p><i ng-if="vm.inputType===\'checkbox\'" ng-show="vm.ngModel" class="fa fa-check fa-2x"></i></div><p class="help-block">{{vm.helpText}}</p></div></div>'
+    template:'<style>\r\n    .popover {\r\n        min-width: 200px;\r\n    }\r\n    \r\n    .input-group {\r\n        width: 100% !important;\r\n    }\r\n    \r\n    .input-group .form-control {\r\n        z-index: 100;\r\n    }\r\n\r\n\r\n\r\n    .checkbox label:after, \r\n.radio label:after {\r\n    content: \'\';\r\n    display: table;\r\n    clear: both;\r\n}\r\n\r\n.checkbox .cr,\r\n.radio .cr {\r\n    position: relative;\r\n    display: inline-block;\r\n    border: 1px solid #a9a9a9;\r\n    border-radius: .25em;\r\n    width: 1.3em;\r\n    height: 1.3em;\r\n    float: left;\r\n    margin-right: .5em;\r\n}\r\n\r\n.radio .cr {\r\n    border-radius: 50%;\r\n}\r\n\r\n.checkbox .cr .cr-icon,\r\n.radio .cr .cr-icon {\r\n    position: absolute;\r\n    font-size: .8em;\r\n    line-height: 0;\r\n    top: 50%;\r\n    left: 20%;\r\n}\r\n\r\n.radio .cr .cr-icon {\r\n    margin-left: 0.04em;\r\n}\r\n\r\n.checkbox label input[type="checkbox"],\r\n.radio label input[type="radio"] {\r\n    display: none;\r\n}\r\n\r\n.checkbox label input[type="checkbox"] + .cr > .cr-icon,\r\n.radio label input[type="radio"] + .cr > .cr-icon {\r\n    transform: scale(3) rotateZ(-20deg);\r\n    opacity: 0;\r\n    transition: all .3s ease-in;\r\n}\r\n\r\n.checkbox label input[type="checkbox"]:checked + .cr > .cr-icon,\r\n.radio label input[type="radio"]:checked + .cr > .cr-icon {\r\n    transform: scale(1) rotateZ(0deg);\r\n    opacity: 1;\r\n}\r\n\r\n.checkbox label input[type="checkbox"]:disabled + .cr,\r\n.radio label input[type="radio"]:disabled + .cr {\r\n    opacity: .5;\r\n}\r\n</style><div class="form-group"><label ng-class="vm.cssClassService.getlabelClass(vm.horizontal, vm.labelWidth)" for="vm.fieldName">{{vm.fieldLabel}}</label><div ng-class="vm.cssClassService.getInputClass(vm.horizontal, vm.inputWidth)"><div ng-if="!vm.readOnly" class="input-group"><input ng-if="vm.inputType===\'textbox\'" ng-model="vm.ngModel" type="text" class="form-control" id="{{vm.fieldName}}" placeholder="{{vm.placeholder}}" required><input ng-if="vm.inputType===\'number\'" ng-model="vm.ngModel" type="number" class="form-control" id="{{vm.fieldName}}" required><input ng-if="vm.inputType===\'email\'" type="email" class="form-control" id="vm.fieldName" placeholder="{{vm.placeholder}}"><textarea ng-if="vm.inputType===\'textarea\'" class="form-control" id="vm.fieldName" ng-model="vm.ngModel" placeholder="{{vm.placeholder}}"></textarea><checkbox large class="btn-default" ng-if="vm.inputType===\'checkbox\'" ng-model="vm.ngModel"></checkbox><select ng-if="vm.inputType===\'select\'" class="form-control" ng-model="vm.ngModel" ng-options="item for item in vm.items"></select><input ng-if="vm.inputType===\'time\'" uib-timepicker ng-model="vm.ngModel" id="vm.fieldName" ng-change="vm.changed()" hour-step="vm.hourStep" minute-step="vm.minStep" show-meridian="vm.showMeridian"><span ng-if="vm.inputType===\'datepopup\'"><input type="text" class="form-control" uib-datepicker-popup="{{vm.format}}" ng-model="vm.ngModel" is-open="vm.isOpened" style="width: 82%" ng-required="true" close-text="Close"> <span class="btn btn-default glyphicon glyphicon-calendar" ng-click="vm.open()"></span></span> <span ng-show="vm.tooltip!==\'\'" class="input-group-addon" uib-popover="{{vm.tooltip}}" popover-title="Info" popover-class="popover" popover-trigger="\'mouseenter\'"><i class="fa fa-info"></i></span></div><div ng-show="vm.readOnly"><p ng-if="vm.inputType!==\'checkbox\'">{{vm.ngModel}}</p><i ng-if="vm.inputType===\'checkbox\'" ng-show="vm.ngModel" class="fa fa-check fa-2x"></i></div><p class="help-block">{{vm.helpText}}</p></div></div>'
 };
 
 myInputField.$inject = ['$scope', 'cssClassService'];
@@ -520,6 +521,22 @@ mySelectField.$inject = ['cssClassService'];
 angular.module('my-angular-components').component('mySimpleTagsField', mySimpleTagsField);
 
 
+angular.module('my-angular-components').directive('markdown', function () {
+    var converter = new Showdown.converter();
+
+
+    var link = function (scope, element, attrs) {
+        attrs.$observe('markdown', function (value) {
+            var markup = converter.makeHtml(value);
+            element.html(markup);
+        });
+    };
+
+    return {
+        restrict: 'A',
+        link: link
+    };
+});
 
 var myRichTextEditor = {
     bindings: {
@@ -541,6 +558,57 @@ var myRichTextEditor = {
 
 
 angular.module('my-angular-components').component('myRichTextEditor', myRichTextEditor);
+
+var myModalButtons = {
+    bindings: {
+        save: '&',
+        close: '&',
+        saveText: '@',
+        saveVisible: '@',
+        closeVisible: '@',
+    },
+    controllerAs: 'vm',
+    controller: function () {
+        var vm = this;
+
+        vm.$onInit = function () {
+            //defaults        
+            vm.saveText = 'Save';
+            vm.closeText = 'Close';
+
+            vm.saveVisible = true;
+            vm.closeVisible = true;
+        };
+
+    },
+    template:'<div class="modal-footer"><span><button class="btn btn-primary btn-large pull-left" ng-if="vm.saveVisible" id="save" ng-click="vm.save()" type="submit">{{vm.saveText}}</button><button class="btn btn-default pull-left" ng-if="vm.closeVisible" id="close" ng-click="vm.close()" type="button">{{vm.closeText}}</button></span></div>'
+};
+
+angular.module('my-angular-components').component('myModalButtons', myModalButtons);
+
+//Todo: Allow customize header size
+var myModalHeader = {
+    bindings: {
+        id: '@',
+        title: '@'
+    },
+    controllerAs: 'vm',
+    controller: function () {
+        var vm = this;
+
+        vm.$onInit = function () {
+            //defaults
+            vm.title = 'Set this text using the title property';
+            vm.id = 'modalHeader';
+        };
+
+
+    },
+    template:'<div class="modal-header" id="vm.id"><h3 class="modal-title">{{vm.title}}</h3></div>'
+};
+
+
+var app = angular.module('my-angular-components').component('myModalHeader', myModalHeader);
 
 var myInfoPanel = {
     bindings: {
@@ -836,57 +904,6 @@ var myStatusAlert = {
 
 
 angular.module('my-angular-components').component('myStatusAlert', myStatusAlert);
-
-var myModalButtons = {
-    bindings: {
-        save: '&',
-        close: '&',
-        saveText: '@',
-        saveVisible: '@',
-        closeVisible: '@',
-    },
-    controllerAs: 'vm',
-    controller: function () {
-        var vm = this;
-
-        vm.$onInit = function () {
-            //defaults        
-            vm.saveText = 'Save';
-            vm.closeText = 'Close';
-
-            vm.saveVisible = true;
-            vm.closeVisible = true;
-        };
-
-    },
-    template:'<div class="modal-footer"><span><button class="btn btn-primary btn-large pull-left" ng-if="vm.saveVisible" id="save" ng-click="vm.save()" type="submit">{{vm.saveText}}</button><button class="btn btn-default pull-left" ng-if="vm.closeVisible" id="close" ng-click="vm.close()" type="button">{{vm.closeText}}</button></span></div>'
-};
-
-angular.module('my-angular-components').component('myModalButtons', myModalButtons);
-
-//Todo: Allow customize header size
-var myModalHeader = {
-    bindings: {
-        id: '@',
-        title: '@'
-    },
-    controllerAs: 'vm',
-    controller: function () {
-        var vm = this;
-
-        vm.$onInit = function () {
-            //defaults
-            vm.title = 'Set this text using the title property';
-            vm.id = 'modalHeader';
-        };
-
-
-    },
-    template:'<div class="modal-header" id="vm.id"><h3 class="modal-title">{{vm.title}}</h3></div>'
-};
-
-
-var app = angular.module('my-angular-components').component('myModalHeader', myModalHeader);
 
 /**
  * Date field component with Field Label, Date Popup, Help Popup
