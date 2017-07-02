e5390f29fd1736505fc5a542a789f641daa08480
Kevin Robinson (FP McCann) <kevin.robinson@outlook.com>
2017-07-02 13:07:54 +0100

    finished Text editor

--- src/client/app/Examples/TextEditor/textEditorExample.js
+++ src/client/app/Examples/TextEditor/textEditorExample.js
@@ -2,13 +2,23 @@ var textEditorExample = {
     controllerAs: 'vm',
     controller: function () {
         var vm = this;
+        //   vm.sampleText = ';; Scheme code in here.\n' +
+        //       '(define (double x)\n\t(* x x))\n\n\n' +
+        //       '<!-- XML code in here. -->\n' +
+        //       '<root>\n\t<foo>\n\t</foo>\n\t<bar/>\n</root>\n\n\n' +
+        //       '// Javascript code in here.\n' +
+        //       'function foo(msg) {\n\tvar r = Math.random();\n\treturn "" + r + " : " + msg;\n}';
+        vm.sampleText = '# Heading 1\n';
+        vm.sampleText += ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus sagittis arcu vel ultricies. \n\n';
 
-        vm.sampleText = "+ item      - subitem" +
-            "The HTML has a superfluous newline before this" +
-            "paragraph." +
-            "- subitem";
+        vm.sampleText += '## Heading 2\n';
+        vm.sampleText += ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus sagittis arcu vel ultricies. \n\n';
+
+        vm.sampleText += '### Heading 3\n';
+        vm.sampleText += ' Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec luctus sagittis arcu vel ultricies. \n\n';
+
     },
-    template:'<my-rich-text-editor ng-model="vm.sampleText"></my-rich-text-editor><div markdown="{{vm.sampleText}}"></div>'
+    templateUrl: './src/client/app/Examples/TextEditor/textEditorExampleTemplate.html'
 };
 
 angular.module('examples').component('textEditorExample', textEditorExample);
