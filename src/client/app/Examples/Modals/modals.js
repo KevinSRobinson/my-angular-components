b313a074c1a9e9c661e103f5b9145fff9a9677fc
Kevin Robinson (FP McCann) <kevin.robinson@outlook.com>
2017-07-02 18:19:43 +0100

    Modals Done

--- src/client/app/Examples/Modals/modals.js
+++ src/client/app/Examples/Modals/modals.js
@@ -5,18 +5,22 @@ var modals = {
 
         vm.open = function () {
             return $uibModal.open({
-                template:'<my-modal-header title="Edit My Contact1"></my-modal-header><div class="modal-body"><my-input-field field-label="FirstName" ng-model="vm.contact.FirstName"></my-input-field><my-input-field field-label="LastName" ng-model="vm.contact.LastName"></my-input-field></div><my-modal-buttons save="alert(\'Saved\')" close="vm.close()"></my-modal-buttons>',
+                templateUrl:'./src/client/app/Examples/Modals/sampleModalTemplate.html',
                 controllerAs: 'vm',
                 controller:function($uibModalInstance){
                     var vm = this;
                     vm.close = function () {
                       $uibModalInstance.close();
                     };
+                     vm.saveContact = function () {
+                      $uibModalInstance.close();
+                    };
                 }
             });
         };
+
     },
-    template:'<div class="example"><h5>Modals</h5><hr><div ng-click="vm.open()" class="btn btn-success">Click To Open</div></div><div class="example"><h1>Modal Helpers</h1><hr><h2>Moreless Panel</h2></div>'
+    templateUrl:'./src/client/app/Examples/Modals/modalsTemplate.html'
 };
 
 angular.module('examples').component('modalExamples', modals);
